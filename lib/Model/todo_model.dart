b6b7c108d4ed82feb13843aba2d88e4f40773a9c
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-01-24 20:11:35 +0500

    Set 1 item as default item

--- lib/Model/todo_model.dart
+++ lib/Model/todo_model.dart
@@ -10,15 +10,7 @@ class ToDoModel {
 
   static List<ToDoModel> allToDoList() {
     return [
-      ToDoModel(id: '1', title: 'Aabis'),
-      ToDoModel(id: '2', title: 'Ahmed', isDone: true),
-      ToDoModel(id: '3', title: 'Hassan', isDone: false),
-      ToDoModel(id: '4', title: 'Aabis', isDone: true),
-      ToDoModel(id: '5', title: 'Ahmed', isDone: false),
-      ToDoModel(id: '6', title: 'Hassan'),
-      ToDoModel(id: '7', title: 'Hassan', isDone: false),
-      ToDoModel(id: '8', title: 'Hassan', isDone: true),
-      ToDoModel(id: '9', title: 'Hassan'),
+         ToDoModel(id: '1', title: 'First ToDo Item'),
     ];
   }
 }
